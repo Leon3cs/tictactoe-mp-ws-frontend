61a1c45b85b1f5ca91237fa2a5171598c967ce23
Rafael <rafael21_psp@hotmail.com>
2023-10-29 23:56:52 -0300

    Format components

--- components/tile.js
+++ components/tile.js
@@ -1,18 +1,17 @@
-import game from '../styles/TicTacToe.module.css'
-import { CROSS } from '../pages/tic-tac-toe'
+import game from "../styles/TicTacToe.module.css";
+import { CROSS } from "../pages/tic-tac-toe";
 
-export default function Tile({value, row, col, handler}){
+export default function Tile({ value, row, col, handler }) {
+  const player = value > 0 ? (value === CROSS ? "X" : "O") : "";
 
-    const player = value > 0 ? (value === CROSS ? 'X' : 'O') : ''
+  const clickHandler = (event) => {
+    handler(row, col);
+    event.preventDefault();
+  };
 
-    const clickHandler = (event) => {
-        handler(row, col)
-        event.preventDefault()
-    }
-
-    return (
-        <div className={game.tile} onClick={clickHandler}>
-            {player}
-        </div>
-    )
+  return (
+    <div className={game.tile} onClick={clickHandler}>
+      {player}
+    </div>
+  );
 }
